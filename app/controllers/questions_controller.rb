2baa24d70b9e3a8427656698ce04d435f2e47862
Angel Gonzalez <angelfabriciogonzalez@gmail.com>
2023-06-18 19:06:56 -0600

    added update a question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,20 @@
 class QuestionsController < ApplicationController
 
-  before_action :set_evaluation, only: %i[create]
+  before_action :set_evaluation, only: %i[create update]
+  before_action :set_question, only: %i[edit update]
+
+
+  def edit 
+  end 
 
   def create
     @question = @evaluation.questions.build(question_params)
 
     respond_to do |format|
-    if @question.save
-        format.turbo_stream { render turbo_stream: turbo_stream.replace('questions_all', partial: 'questions/questions', locals: { evaluation: @evaluation }) }
+    if @question.save 
+        respond_to do |format|
+          format.turbo_stream { render turbo_stream: turbo_stream.replace('questions_all', partial: 'questions/questions', locals: { evaluation: @evaluation }) }
+        end 
       else 
         format.html { render :new, status: :unprocessable_entity }
       end 
@@ -15,6 +22,13 @@ class QuestionsController < ApplicationController
   end
 
   def update
+     if @question.update(question_params)
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("question_#{@question.id}", partial: 'questions/question', locals: { question: @question }) }
+      end 
+    else 
+      format.html { render :edit, status: :unprocessable_entity }
+    end 
   end
 
   def destroy
@@ -28,5 +42,9 @@ class QuestionsController < ApplicationController
 
     def question_params 
       params.require(:question).permit(:name, :question_type)
+    end 
+
+    def set_question 
+        @question = Question.find(params[:id])
     end 
 end
